60435a759c20e5d72d7c61b9cc0cc77d088a02a5
Magdalena Pölder <magdap@rackham3.uppmax.uu.se>
2022-04-01 10:45:30 +0200

    updating ignore files

--- analyses/01_genome_assembly/correction/0-mercounts/meryl-count.jobSubmit-01.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sbatch \
-  --cpus-per-task=4 --mem-per-cpu=768m -o meryl-count.%A_%a.out \
-  -D `pwd` -J "meryl_genome_assembly" \
-  -a 1-1 \
-  `pwd`/meryl-count.sh 0 \
-> ./meryl-count.jobSubmit-01.out 2>&1
